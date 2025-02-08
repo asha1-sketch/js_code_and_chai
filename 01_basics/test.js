0b29e801b986fb97c7f81e17af3365abe23fc864
asha1-sketch <asha.qwerty.891@gmail.com>
2025-02-08 18:26:13 +0530

    change

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,4 +1,4 @@
-console.log("Asha")
+/*console.log("Asha")
 const accoundId = 14225
 let accountEmail="asha@gmail.com"
 var accountPassword= "12356"
@@ -9,3 +9,32 @@ accountEmail="asha.1@gmail.com"
 accountPassword="14789"
 accountCity="jaipur"
 console.table([accoundId,accountEmail,accountPassword,accountCity])
+//alert("hello"); //we are using nodejs not browser
+let ok=123;
+console.log(ok);
+console.log(typeof(null));
+console.log(typeof(undefined));
+*/
+let score="45ab"
+let score1=Number(score)
+console.log(typeof(score1));
+console.log(score1);
+let asha=123
+let asha1=String(asha)
+console.log(typeof asha1);
+console.log(-asha);
+console.log(2**3);
+console.log("asha" +" sharma");
+console.log("1" +2);
+console.log(1+"2"+2);
+let num1,num2,num3
+num1=num2=num3=4
+++asha
+console.log(asha);
+console.log("2">1);
+console.log(null>0);
+console.log(null>=0);
+console.log(null==0);
+console.log(undefined==0);
+console.log(undefined>=0);
+console.log("2"==2);
