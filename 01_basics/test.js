300ba066390e49f41cc741b632cf36c4fac37751
asha1-sketch <asha.qwerty.891@gmail.com>
2025-02-08 13:49:54 +0530

    change

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,11 @@
 console.log("Asha")
+const accoundId = 14225
+let accountEmail="asha@gmail.com"
+var accountPassword= "12356"
+accountCity="panipat"
+//accoundId=142256
+console.log(accoundId);
+accountEmail="asha.1@gmail.com"
+accountPassword="14789"
+accountCity="jaipur"
+console.table([accoundId,accountEmail,accountPassword,accountCity])
