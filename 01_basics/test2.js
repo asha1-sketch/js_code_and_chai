28c7cec8ba483ae6b8ba8d38766d4d851c08b306
asha1-sketch <asha.qwerty.891@gmail.com>
2025-02-15 16:27:27 +0530

    math

--- 01_basics/test2.js
+++ 01_basics/test2.js
@@ -1,5 +1,6 @@
-//stach and heap memory
-let name="asha";
+//stack and heap memory
+
+/*let name="asha";
 let anothername=name
 anothername="guddu"
 console.log(name);
@@ -7,7 +8,7 @@ console.log(anothername);
 
 let arr=[1,2,3,4,5]
 let arr1=arr
-arr1=[6,7,8,9]
+arr1[2]=566
 console.log(arr);
 console.log(arr1);
 
@@ -19,3 +20,34 @@ let objj=obj
 objj.name="prem"
 console.log(obj);
 console.log(objj);
+console.log("hello " + "world");
+console.log(`${name} is btech(ai&ds) student.`);
+let mom=new String("sush")
+console.log(mom.toUpperCase());
+console.log(mom.length);
+console.log(mom.replace("sush","sushma"));
+console.log(mom);
+let statement="hi.to.all.my.name.is.asha.k"
+console.log(statement.trim());
+console.log(statement.split("."));
+console.log(statement.slice(0,5));//here you can give negative value
+console.log(statement.substring(0,5));
+console.log(statement.slice(-3));
+let a ="        ohlh    ojo     "
+console.log(a.trim());
+console.log("asha sharma".replace("asha","prem"));
+console.log(statement.includes("i.t"));
+*/
+const balance=new Number(2516.8956)
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+console.log(balance.toPrecision(3));
+console.log(balance.toLocaleString());//separate using commas
+console.log(Math.PI);
+console.log(Math.floor(balance));
+console.log(Math.abs(-56));
+console.log(Math.sqrt(balance));
+console.log(Math.random());//Returns a pseudorandom number between 0 and 1
+console.log(Math.round((Math.random())*10+1));
+
