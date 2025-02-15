f0fbce4fa84e47449fda05a6edefef458eb9ea38
asha1-sketch <asha.qwerty.891@gmail.com>
2025-02-15 12:46:21 +0530

    ok

---
+++ 01_basics/test2.js
@@ -0,0 +1,21 @@
+//stach and heap memory
+let name="asha";
+let anothername=name
+anothername="guddu"
+console.log(name);
+console.log(anothername);
+
+let arr=[1,2,3,4,5]
+let arr1=arr
+arr1=[6,7,8,9]
+console.log(arr);
+console.log(arr1);
+
+let obj={
+    name:"asha",
+    age:20
+}
+let objj=obj
+objj.name="prem"
+console.log(obj);
+console.log(objj);
