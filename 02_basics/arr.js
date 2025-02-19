f7a3968caaae86b92023766ef102406d208d32e2
asha1-sketch <asha.qwerty.891@gmail.com>
2025-02-20 00:18:58 +0530

    arrayObject

--- 02_basics/arr.js
+++ 02_basics/arr.js
@@ -5,4 +5,40 @@ myarr.pop()
 console.log(myarr);
 myarr.reverse()
 console.log(myarr);
-myarr.slice()
+console.log(myarr.slice(0,3));
+myarr.unshift(123456)
+console.log(myarr);
+myarr.shift()
+console.log(myarr);
+const newarr =myarr.join("#")
+console.log(newarr);
+const my=myarr.slice(0,3)
+console.log(my);
+console.log(myarr.slice(0,3));//retruns a copy
+console.log(myarr);
+const c=myarr.splice(0,3)//permnentely deletes from original array
+console.log(c);
+console.log(myarr);
+const a=["asha","prem"]
+const s=["sharma"]
+console.log(a.concat(s));
+const neew=[...a,...s]
+console.log(neew);
+console.log(Array.isArray("asha"));
+console.log(Array.from("asha"));
+const aa=10
+const bb=20
+const cc=30
+console.log(Array.of(aa,bb,cc));
+const sy=Symbol("okok")
+//object literals
+const person={
+    name:"asha",
+    age:20,
+    city:"Panipat",
+    email:"asha@gmail.com",
+    [sy]:"asasas"
+}
+console.log(person.name);
+console.log(person["name"]);//agar square bracket se access karna hai to key ko string leke chalna padega
+console.log(person[sy]);
